e13b53b93742312ba48e1cf80134ffb3a4d13df9
270122090 <81940094+270122090@users.noreply.github.com>
2021-10-04 18:08:31 +1300

    Password
    creating a login

---
+++ small/mainwindow.cpp
@@ -0,0 +1,39 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "dialog2.h"
+#include <QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_pblogin_clicked()
+{
+    QString username = ui->txtuser->text();
+    QString password = ui->txtpass->text();
+
+    if (username == "admin" && password == "1234")
+    {
+        QMessageBox::information(this,"login", "You have logged in succesful"); // ::information(this,"Login Successfull");
+        hide();
+        second = new Dialog2(this);
+        second->show();
+    }
+    else
+    {
+        QMessageBox::warning(this, "login", "failed");// you have to give your box login
+    }
+
+
+
+}
+
